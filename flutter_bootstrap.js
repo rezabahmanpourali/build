3368eeb1685ca51558b8861137c37e02b366d1b3
reza bahmanpourali <wwwzcv1234@gmail.com>
2025-10-27 23:29:40 +0330

    ....

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3595008832"
+    serviceWorkerVersion: "2410147222"
   }
 });
